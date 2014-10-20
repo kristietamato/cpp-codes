7695dc6f63799089848fa95a6fbb799bc44ff7ae
Kristie Nguyen <nguyenkristie@live.com>
2014-10-20 12:26:39 -0700

    created inputPerson1, getInt, and main stuff

--- Project06-Functions/Project06-Functions/main.cpp
+++ Project06-Functions/Project06-Functions/main.cpp
@@ -1,31 +1,72 @@
 //  Created by Kristie Nguyen on 10/20/14.
-//  A program that converts height and gets its average out of 10 and prints this data.
+//  A program that converts height and gets its average out of 10 then prints these data.
 
 #include <iostream>
 #include <string>
 using namespace std;
 
 int const INCHES_IN_FOOT = 12;
+int const TOTAL_INPUTS = 10;
 
 // function prototypes
 void pause(void);
-void inputHeight_Inches(int inches);
+void inputPerson1(string &name, int &inches);
 float calculateInchesToFeet(int inches, float feet);
 float calculateAverageHeight_Inches(int inches);
 void outputDataTable(int inches, float feet);
-void outputAverageHeight(float height);
+void outputAverageHeight(int averageHeight_Inches);
 void flushInput(void);
 
 int main(void)
 {
-
-
+    try
+    {
+        runStuff();
+    }
+    catch (int exceptionValue)
+    {
+        cout << "Datas out of bound. Program closing." << endl;
+        pause()
+        return exceptionValue;
+    }
+    pause();
     return 0;
 }
 
+int getInt(string prompt)
+{
+    cout << prompt;
+    int userInput;
+    cin >> userInput;
+    flushInput();
+    if (cin.fail())
+    {
+        cin.clear();
+        flushInput();
+        cout << "Improper data input." << endl;
+        throw 1;
+    }
+    return userInput;
+}
+
+void inputPerson1(string &name, int &inches);
+{
+    name = getName("Enter name 01: ");
+    inches = getInches("Enter height 01: ");
+}
 float calculateInchesToFeet(int inches, float feet)
 {
     return feet = inches / INCHES_IN_FOOT;
+}
+
+float calculateAverageHeight(int inches)
+{
+    return averageHeight_Inches = sumOfHeights_Inches / TOTAL_INPUTS;
+}
+
+void outputAverageHeight(int averageHeight_Inches);
+{
+    cout << "Average height is " << averageHeight_Inches << "inches" << endl;
 }
 
 void pause(void)
