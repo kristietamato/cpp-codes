75b060a692624c685bb23f36bfe85301caa4b6dd
Kristie Nguyen <nguyenkristie@live.com>
2014-10-20 11:25:09 -0700

    added function prototypes

--- Project06-Functions/Project06-Functions/main.cpp
+++ Project06-Functions/Project06-Functions/main.cpp
@@ -1,11 +1,22 @@
 //  Created by Kristie Nguyen on 10/20/14.
-//  A program that converts height and gets its average out of 5 and prints this data.
+//  A program that converts height and gets its average out of 10 and prints this data.
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+// function prototypes
+void pause(void);
+void inputHeight_Inches(int inches);
+float calculateInchesToFeet(int inches, float feet);
+float calculateAverageHeight(float average);
+void outputDataTable(int inches, float feet);
+void outputAverageHeight(float height);
+void flushInput(void);
 
 int main(void)
 {
-    //
+
 
     return 0;
 }
