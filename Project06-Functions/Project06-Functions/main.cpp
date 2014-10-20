789cc98f9154719d0a37f0b767671dbca99fbdd7
Kristie Nguyen <nguyenkristie@live.com>
2014-10-20 11:54:08 -0700

    added caluculateInchesToFeet functions and other tools

--- Project06-Functions/Project06-Functions/main.cpp
+++ Project06-Functions/Project06-Functions/main.cpp
@@ -5,11 +5,13 @@
 #include <string>
 using namespace std;
 
+int const INCHES_IN_FOOT = 12;
+
 // function prototypes
 void pause(void);
 void inputHeight_Inches(int inches);
 float calculateInchesToFeet(int inches, float feet);
-float calculateAverageHeight(float average);
+float calculateAverageHeight_Inches(int inches);
 void outputDataTable(int inches, float feet);
 void outputAverageHeight(float height);
 void flushInput(void);
@@ -19,4 +21,19 @@ int main(void)
 
 
     return 0;
+}
+
+float calculateInchesToFeet(int inches, float feet)
+{
+    return feet = inches / INCHES_IN_FOOT;
+}
+
+void pause(void)
+{
+	cout << "Press ENTER to continue...";
+    cin.ignore(999,'\n');
+}
+void flushInput(void)
+{
+    cin.ignore(999,'\n');
 }
