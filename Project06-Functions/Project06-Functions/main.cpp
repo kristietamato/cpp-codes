4664b31f359e7fbab3c7ee84ef17490d0521a4f0
Kristie Nguyen <nguyenkristie@live.com>
2014-10-20 10:08:37 -0700

    created project 6

---
+++ Project06-Functions/Project06-Functions/main.cpp
@@ -0,0 +1,11 @@
+//  Created by Kristie Nguyen on 10/20/14.
+//  A program that converts height and gets its average out of 5 and prints this data.
+
+#include <iostream>
+
+int main(void)
+{
+    //
+
+    return 0;
+}
