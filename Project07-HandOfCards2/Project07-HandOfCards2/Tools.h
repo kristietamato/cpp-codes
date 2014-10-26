2a7ca8ee597a0ab5f641b3d9210c0c4b9c892862
Kristie Nguyen <nguyenkristie@live.com>
2014-10-26 11:13:15 -0700

    started hand of cards 2

--- Project07-HandOfCards2/Project07-HandOfCards2/Tools.h
+++ Project07-HandOfCards2/Project07-HandOfCards2/Tools.h
@@ -3,9 +3,15 @@
 #include<iostream>
 using namespace std;
 
+// input range
+const int MIN_CARD_NORMAL_VALUE	= 1;
+const int MAX_CARD_NORMAL_VALUE	= 13;
+
 #ifndef TOOLS_LOCK
 #define TOOLS_LOCK
 
-
+void formatCheck(void);
+void rangeCheck( int card1, int card2, int card3 );
+void pause(void);
 
 #endif
--- Project07-HandOfCards2/Project07-HandOfCards2/main.cpp
+++ Project07-HandOfCards2/Project07-HandOfCards2/main.cpp
@@ -6,3 +6,28 @@
 
 #include "Tools.h"
 #include "Play.h"
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(void)
+{
+    try
+    {
+        Play();
+    }
+    catch ( int exceptionValue )
+    {
+        cout << "dying now..." <<endl;
+        cout << "Press ENTER to finish...";
+        cin.ignore(999,'\n');
+        return exceptionValue;
+    }
+    
+    // 4 finish up
+    cout << endl;
+    cout << "Press ENTER to finish...";
+    cin.ignore(999,'\n');
+    return 0;
+}
